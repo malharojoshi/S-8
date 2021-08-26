b23e77221c3c8ab7b49133f927e12706f067e3f5
malharojoshi <73136359+malharojoshi@users.noreply.github.com>
2021-08-25 22:26:33 -0400

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    Image
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import PostCard from "./PostCard";
+import firebase from 'firebase'
+import { FlatList } from "react-native-gesture-handler";
+
+let posts = require("./temp_posts.json");
+
+
+export default class Feed extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          posts:""
+        };
+    }
+
+    componentDidMount() {
+      this.fetchStories();
+      this.fetchUser();
+    }
+
+    renderItem = ({ item: post }) => {
+        return <PostCard post={post} navigation={this.props.navigation} />;
+    };
+
+    keyExtractor = (item, index) => index.toString();
+
+    fetchPosts = () => {
+    firebase
+      .database()
+      .ref("/posts/")
+      .on(
+        "value",
+        snapshot => {
+          let posts = [];
+          if (snapshot.val()) {
+            Object.keys(snapshot.val()).forEach(function (key) {
+              posts.push({
+                key: key,
+                value: snapshot.val()[key]
+              });
+            });
+          }
+          this.setState({ posts: posts });
+          this.props.setUpdateToFalse();
+        },
+        function (errorObject) {
+          console.log("The post failed: " + errorObject.code);
+        }
+      );
+  };
+
+  fetchUser = () => {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", snapshot => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === "light" });
+      });
+  };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={styles.appTitle}>
+                    <View style={styles.appIcon}>
+                        <Image
+                            source={require("../assets/logo.png")}
+                            style={styles.iconImage}
+                        ></Image>
+                    </View>
+                    <View style={styles.appTitleTextContainer}>
+                        <Text style={styles.appTitleText}>Spectagram</Text>
+                    </View>
+                </View>
+                <View style={styles.cardContainer}>
+                {!this.state.posts[0] ? (
+            <View style={styles.noPosts}>
+              <Text
+                style={
+                  this.state.light_theme
+                    ? styles.noPostsTextLight
+                    : styles.noPostsText
+                }
+              >
+                No Posts Available
+              </Text>
+            </View>
+          ) : (
+            <View style={styles.cardContainer}>
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={this.state.posts}
+                renderItem={this.renderItem}
+              />
+            </View>
+          )}
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.8,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28),
+    },
+    cardContainer: {
+        flex: 0.85
+    }
+});
